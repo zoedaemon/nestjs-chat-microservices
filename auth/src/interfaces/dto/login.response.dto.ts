f6713f44e72cd3a002dcd23ea978dbb26420b680
Roy Adventus <zoedaemon@users.noreply.github.com>
2024-02-18 01:32:56 +0700

    bugfix token not hold userId and added id in the response DTO

--- auth/src/interfaces/dto/login.response.dto.ts
+++ auth/src/interfaces/dto/login.response.dto.ts
@@ -2,8 +2,14 @@ import { IsString, IsEmail, IsNotEmpty, MinLength, Length } from 'class-validato
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginResponseDto {
-  @ApiProperty()
+  @ApiProperty({ description: '@access_token will be used to access other microservices, will be expired on 1 hour' })
   @IsNotEmpty()
   @IsString()
   access_token: string;
+
+  
+  @ApiProperty({ description: '@id is userId, will match with related @access_token' })
+  @IsNotEmpty()
+  @IsString()
+  id: string;
 }
